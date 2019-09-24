3cb323b70398fa07693512afc6d4b90dc0fed3e3
vol4tim <vol4tim@gmail.com>
2019-09-24 15:19:04 +0400

    upd news block

--- client/src/components/blocks/News.vue
+++ client/src/components/blocks/News.vue
@@ -1,26 +1,5 @@
 <template>
   <v-card>
-    <v-card-title>{{$t('news.publications')}}</v-card-title>
-    <v-list-item two-line v-for="(item, i) in list" :key="i">
-      <v-list-item-icon v-if="item.icon">
-        <v-icon>mdi-alert-circle-outline</v-icon>
-      </v-list-item-icon>
-      <v-list-item-content>
-        <v-list-item-subtitle>{{item.pubDate}}</v-list-item-subtitle>
-        <v-list-item-content>
-          <a :href="item.link" target="_blank">{{item.title}}</a>
-        </v-list-item-content>
-      </v-list-item-content>
-    </v-list-item>
+    <v-card-title>Құттықтау сөз</v-card-title>
   </v-card>
 </template>
-
-<script>
-import { mapGetters } from "vuex";
-
-export default {
-  computed: {
-    ...mapGetters("news", ["list"])
-  }
-};
-</script>
