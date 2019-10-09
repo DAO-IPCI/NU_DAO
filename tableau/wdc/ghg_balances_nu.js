7fed65b383e03f6b66fbc51d22e817f279521bba
Alisher A. Khassanov <a.khssnv@gmail.com>
2019-10-09 06:40:00 +0200

    Block number for WDC data

--- tableau/wdc/ghg_balances_nu.js
+++ tableau/wdc/ghg_balances_nu.js
@@ -21,9 +21,20 @@
 
     myConnector.getData = function(table, doneCallback) {
         tableau.log("Fetching data");
-        var server = "hbrz.khassanov.me:80";
+        var server = "nudao.kz";
         var today = new Date();
         var month = ("0" + (today.getMonth())).slice(-2) + "/" + today.getFullYear(); // prev month
+        var member_block_numbers = [];
+        $.getJSON("http://" + server + "/api/v1/members", function(resp) {
+            tableau.log("Fetching members list");
+            for (var i = 0, len = resp.length, i < len; i++) {
+                member_block_numbers.push({
+                    "member_id": resp[i].id,
+                    "member_block_number": parseInt(resp[i].name.split(" ")[1]);
+                });
+            }
+        });
+
         $.getJSON("http://" + server + "/api/v1/members/ghg/balances?month=" + month, function(resp) {
             tableau.log("Recieved:");
             tableau.log(resp);
@@ -31,8 +42,12 @@
             var tableData = [];
 
             for (var i = 0, len = balances.length; i < len; i++) {
+                var member_block_number = member_block_numbers.find(function(elem) {
+                    return elem.member_id == balances[i].member_id;
+                }).member_block_number;
                 tableData.push({
                     "member_id": balances[i].member_id,
+                    "member_block_number": member_block_numbers.f
                     "balance": balances[i].balance
                 });
             }
