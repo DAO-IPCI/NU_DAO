b21aaf6e25bea261b47fc3a07202045d863e11ec
Alisher A. Khassanov <a.khssnv@gmail.com>
2019-10-09 11:37:54 +0200

    WDC fixes

--- tableau/wdc/ghg_balances_nu.js
+++ tableau/wdc/ghg_balances_nu.js
@@ -9,6 +9,9 @@
         }, {
             id: "balance",
             dataType: tableau.dataTypeEnum.int
+        }, {
+            id: "member_block_number",
+            dataType: tableau.dataTypeEnum.int
         }];
 
         var tableSchema = {
@@ -20,34 +23,28 @@
     };
 
     myConnector.getData = function(table, doneCallback) {
-        tableau.log("Fetching data");
         var server = "nudao.kz";
         var today = new Date();
         var month = ("0" + (today.getMonth())).slice(-2) + "/" + today.getFullYear(); // prev month
         var member_block_numbers = [];
-        $.getJSON("http://" + server + "/api/v1/members", function(resp) {
-            tableau.log("Fetching members list");
-            for (var i = 0, len = resp.length, i < len; i++) {
+        $.getJSON("https://" + server + "/api/v1/members", function(resp) {
+            for (var i = 0, len = resp.length; i < len; i++) {
                 member_block_numbers.push({
-                    "member_id": resp[i].id,
-                    "member_block_number": parseInt(resp[i].name.split(" ")[1]);
+                    "member_id": parseInt(resp[i].id),
+                    "member_block_number": parseInt(resp[i].name.split(" ")[1])
                 });
             }
         });
 
-        $.getJSON("http://" + server + "/api/v1/members/ghg/balances?month=" + month, function(resp) {
-            tableau.log("Recieved:");
-            tableau.log(resp);
+        $.getJSON("https://" + server + "/api/v1/members/ghg/balances?month=" + month, function(resp) {
             var balances = resp.balances;
             var tableData = [];
-
             for (var i = 0, len = balances.length; i < len; i++) {
-                var member_block_number = member_block_numbers.find(function(elem) {
-                    return elem.member_id == balances[i].member_id;
-                }).member_block_number;
                 tableData.push({
                     "member_id": balances[i].member_id,
-                    "member_block_number": member_block_numbers.f
+                    "member_block_number": member_block_numbers.find(function(elem) {
+                      return elem.member_id == parseInt(balances[i].member_id);
+                    }).member_block_number,
                     "balance": balances[i].balance
                 });
             }
@@ -65,5 +62,4 @@
         tableau.connectionName = "NU GHG Balance";
         tableau.submit();
     };
-
 })();
